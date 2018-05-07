0b4be92b5d06795d3f30e1d452e3332e6897c7ba
smf <silviamourelof@gmail.com>
2018-05-07 17:31:03 +0200

    añadido main js y css

--- jasmine-standalone-3.1.0/spec/mainSpec.js
+++ jasmine-standalone-3.1.0/spec/mainSpec.js
@@ -17,45 +17,180 @@
 * */
 
 
-describe('calculo de marcador', function(){
-
-  function recalcularMarcador(puntos, esCorrecta, tiempo) {
-    if(esCorrecta) {
-      if(tiempo <= 2) {
-        return puntos + 2;
-      } else if(tiempo < 10 && tiempo > 2) {
-        return puntos + 1;
-      } else { 
-        return puntos;
+describe('calculo de marcador', function() {
+  function recalcularAcertandoPregunta(marcador, tiempo) {
+      if (tiempo <= 2) {
+          return marcador + 2;
       }
-    } else {
-      if(tiempo < 10) {
-        return puntos - 1;
-      } else if(tiempo > 10 && tiempo < 20) {
-        return puntos - 2;
-      } else {
-        return puntos - 3;
+      if (tiempo <= 10) {
+          return marcador + 1;
       }
-    }
+      if (tiempo > 10){
+          return marcador;
+      }
+  }
+  function recalcularFallandoPregunta(marcador, tiempo) {
+      if (tiempo <= 10) {
+          return marcador - 1;
+      }
+      if (tiempo < 20) {
+          return marcador - 2;
+      }
+  }
+  function recalcularSinRespuesta(marcador) {
+      return marcador - 3;
   }
 
-  it("suma mas puntos si acierta muy rapido", function(){
-      expect(recalcularMarcador(0, true, 1)).toBe(2);
-      expect(recalcularMarcador(2, true, 1)).toBe(4);
+  it("suma mas puntos si acierta en t <= 2", function() {
+      expect(recalcularAcertandoPregunta(0, 1)).toBe(2);
+      expect(recalcularAcertandoPregunta(2, 1)).toBe(4);
   });
-
-  it("resto puntos si fallo en función del tiempo que tarde en responder", function(){
-    expect(recalcularMarcador(5, false, 12)).toBe(3);
-    expect(recalcularMarcador(7, false, 8)).toBe(6);
+  it("suma menos puntos si acierta en t > 2 y t <= 10", function() {
+      expect(recalcularAcertandoPregunta(0, 5)).toBe(1);
+      expect(recalcularAcertandoPregunta(2, 10)).toBe(3);
   });
-
-  it("puntos si acierto", function(){
-    expect(recalcularMarcador(5, true, 6)).toBe(6);
-    expect(recalcularMarcador(1, true, 15)).toBe(1);
+  it("suma menos puntos si acierta en t > 10", function() {
+      expect(recalcularAcertandoPregunta(0, 11)).toBe(0);
+      expect(recalcularAcertandoPregunta(2, 11)).toBe(2);
   });
-
-  it("es obligatorio responder a una pregunta", function(){
-    expect(recalcularMarcador(5, null, 20)).toBe(2);
+  it("resta menos puntos si pierde en t <= 10", function() {
+      expect(recalcularFallandoPregunta(0, 5)).toBe(-1);
+      expect(recalcularFallandoPregunta(2, 10)).toBe(1);
   });
- 
+  it("resta mas puntos si pierde en < 20", function() {
+      expect(recalcularFallandoPregunta(0, 11)).toBe(-2);
+      expect(recalcularFallandoPregunta(2, 18)).toBe(0);
+  });
+  it("resta puntos si no hay respuesta", function() {
+      expect(recalcularSinRespuesta(0)).toBe(-3);
+      expect(recalcularSinRespuesta(20)).toBe(17);
+  });
 });
+
+
+/* 
+
+describe('comprobador de respuestas', function() {
+  function isCorrect(question, userSelectedAnswer) {
+      if(question.id !== userSelectedAnswer.questionId) {
+          return false;
+      }
+      if (userSelectedAnswer.id !== question.correctAnswer.id) {
+          return false;
+      }
+      return true;
+  }
+  it("reconoce una pregunta correcta", function() {
+      expect(isCorrect({
+                      id: 1,
+                      question: '¿Cual es la capital de Portugal?',
+                      answers: [
+                          {id: 1, value: 'Faro'},
+                          {id: 2, value: 'Oporto'},
+                          {id: 3, value: 'Lisboa'},
+                      ],
+                      correctAnswer: {id: 3}
+                  },
+                  {questionId: 1, id: 3 })
+          ).toBeTruthy();
+      });
+  it("reconoce una pregunta incorrecta", function() {
+      expect(isCorrect({
+                      id: 1,
+                      question: '¿Cual es la capital de Portugal?',
+                      answers: [
+                          {id: 1, value: 'Faro'},
+                          {id: 2, value: 'Oporto'},
+                          {id: 3, value: 'Lisboa'},
+                      ],
+                      correctAnswer: {id: 3}
+                  },
+                  {questionId: 1, id: 2 })
+      ).toBeFalsy();
+  });
+
+  it("reconoce una respuesta que no corresponde a la pregunta", function() {
+      expect(isCorrect({
+                  id: 1,
+                  question: '¿Cual es la capital de Portugal?',
+                  answers: [
+                      {id: 1, value: 'Faro'},
+                      {id: 2, value: 'Oporto'},
+                      {id: 3, value: 'Lisboa'},
+                  ],
+                  correctAnswer: {id: 3}
+              },
+              {questionId: 5, id: 3 })
+      ).toBeFalsy();
+  });
+}); */
+
+
+describe('comprobador de respuestas', function() {
+  var question = {
+    id: 1,
+    question: '¿Cual es la capital de Portugal?',
+    answers: [
+        {id: 1, value: 'Faro'},
+        {id: 2, value: 'Oporto'},
+        {id: 3, value: 'Lisboa'},
+    ],
+    correctAnswer: 3
+  };
+
+  it("reconoce una pregunta correcta", function() {
+      expect(isCorrectAnswer(question, 3)).toBeTruthy();
+  });
+
+  it("reconoce una pregunta incorrecta", function() {
+      expect(isCorrectAnswer(question, 2)).toBeFalsy();
+  });
+
+  it("reconoce una respuesta que se correponde con la pregunta", function() {
+      const answer = {
+        questionId: 1,
+        answerId: 3
+      };
+
+      expect(isValidAnswer(question, answer)).toBeTruthy();
+  });
+  
+  it("reconoce una respuesta que no corresponde a la pregunta", function() {
+    const answer = {
+      questionId: 2,
+      answerId: 3
+    };
+    
+    expect(isValidAnswer(question, answer)).toBeFalsy();
+  });
+});
+
+
+describe('mostrar preguntas', function() {
+    var questions;
+    beforeEach (function() {
+        spyOn(console, "log");
+        getQuestions(function(data){
+          questions = data;
+        });
+    });
+
+    it("mostrar la pregunta con un determinado id", function() {
+
+        pickQuestion(questions, 3);
+        expect(console.log).toHaveBeenCalled();
+        // expect(console.log).toHaveBeenCalledWith('¿Cual es la capital de Italia?');
+    });
+
+    it("mostrar pregunta aleatoria", function() {
+
+        pickQuestion(questions);
+        expect(console.log).toHaveBeenCalled();
+    });
+  
+  });
+  
+  
+
+
+
--- main.js
+++ main.js
@@ -1,29 +1,211 @@
-// let totalPrice = 0;
-// let pricePhone = 10;
-// let savings = 40;
-// const addButton = document.querySelector('button');
-
-// addButton.addEventListener('click', function () {
-//     totalPrice += pricePhone
-//     compareMoney(totalPrice, savings)
-// });
-
-// const compareMoney = (a, b) => {
-//     if (a <= b) {
-//         console.log('buy the phone')
-//     } else {
-//         console.log('no phone')
-//     }
-// }
-
-// function addPhone() { }
-function canIBuy(totalPrice, savings, itemPrice) {
-    return ((totalPrice + itemPrice) < savings);
+function recalcularAcertandoPregunta(marcador, tiempo) {
+    if (tiempo <= 2) {
+        return marcador + 2;
+    }
+    if (tiempo <= 10) {
+        return marcador + 1;
+    }
+    if (tiempo > 10){
+        return marcador;
+    }
 }
 
-function sumItems(itemPrice, totalPrice, savings){
-    if(canIBuy(totalPrice, savings, itemPrice)){
-        return (totalPrice += itemPrice);
+
+function recalcularFallandoPregunta(marcador, tiempo) {
+    if (tiempo <= 10) {
+        return marcador - 1;
     }
-    return totalPrice; 
+    if (tiempo < 20) {
+        return marcador - 2;
+    }
+}
+
+function recalcularSinRespuesta(marcador) {
+    return marcador - 3;
+}
+
+
+
+//   const questions = [
+//       {
+//         id: 1,
+//         question: '¿Cual es la capital de Portugal?',
+//         answers: [
+//             {id: 1, value: 'Faro'},
+//             {id: 2, value: 'Oporto'},
+//             {id: 3, value: 'Lisboa'},
+//         ],
+//         correctAnswer: 3
+//     }, 
+    
+//     {
+//         id: 2,
+//         question: '¿Cual es la capital de Francia?',
+//         answers: [
+//             {id: 1, value: 'Faro'},
+//             {id: 2, value: 'Paris'},
+//             {id: 3, value: 'Roma'},
+//         ],
+//         correctAnswer: 2
+//     },
+
+//     {
+//         id: 3,
+//         question: '¿Cual es la capital de Italia?',
+//         answers: [
+//             {id: 1, value: 'Roma'},
+//             {id: 2, value: 'Paris'},
+//             {id: 3, value: 'Madrid'},
+//         ],
+//         correctAnswer: 1
+//     }
+// ]
+
+// La defincion de la funcion, copiar y pegar en vuestro codigo:
+function getQuestions(callback){
+    var serverData = [
+      {
+        question: {id: 1, text: 'Pregunta1'},
+        answers: [{id: 1, text: 'A1'}, {id: 2, text: 'A2'}, {id: 3, text: 'A3'}],
+        correctAnswerId: 1
+        },
+      {
+        question: {id: 2, text: 'Pregunta2'},
+        answers: [{id: 4, text: 'B1'}, {id: 5, text: 'B2'}, {id: 6, text: 'B3'}],
+        correctAnswerId: 5
+        },
+      {
+        question: {id: 3, text: 'Pregunta3'},
+        answers: [{id: 7, text: 'C1'}, {id: 8, text: 'C2'}, {id: 9, text: 'C3'}],
+        correctAnswerId: 9
+        },          
+    ]
+    callback(serverData);
+};
+// La teneis que usar pasando un callback, o sea una funcion:
+// Ejemplo de uso:
+
+
+  function pickQuestion(questions, questionId) {
+    var id = questionId || 1 + Math.round(Math.random() * (questions.length - 1));
+
+    for(let question of questions) {
+        if(question.question.id === id) {
+            // console.log(showQuestion(question));
+            return question;
+            // showAnswers(question.answers, true);
+            // break;    
+        }
+    }
+  }
+
+  function showQuestion(question) {
+      return question.question.text;
+  }
+
+
+  function showAnswers(answers, random) {
+
+    if(random) {
+        showRandomAnswers(answers);
+    } else {
+        for(let answer of answers) {
+            console.log(answer.text);
+        }
+    }
+  }
+
+  function showRandomAnswers(answers) {
+
+      var randomIndex;
+      var answersLeft = answers.slice(0);
+
+      while(answersLeft.length > 0) {
+          randomIndex = Math.floor(Math.random() * answersLeft.length);
+          console.log(answersLeft.splice(randomIndex, 1)[0].text);
+      }
+  }
+
+  function shuffle(array) {
+    let shuffledArray = array.slice(0);
+    let counter = shuffledArray.length;
+
+
+    while(counter > 0){
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+        [shuffledArray[counter], shuffledArray[index]] = [shuffledArray[index], shuffledArray[counter]];
+    }
+    return shuffledArray;
+  }
+
+  function isCorrectAnswer(question, userSelectedAnswer) {
+      return userSelectedAnswer === question.correctAnswer;
+  }
+
+  function isValidAnswer(question, userSelectedAnswer) {
+    return question.id === userSelectedAnswer.questionId;
+  }
+
+function start() {
+    var questions, intervalID;
+    getQuestions((data) => {
+        questions = shuffle(data)
+    })
+
+    printQuestion(questions.pop());
+    // intervalID = setInterval(() => {
+    //     if(questions.length > 0) {
+    //         printQuestion(questions.pop())
+    //     }
+    //     else {
+    //         stop(intervalID);
+    //     }
+
+    // }, 5000)
+}
+
+function stop(intervalID) {
+    document.querySelectorAll('.main-questions form *').forEach(item=>item.remove())
+    document.querySelector('.question').innerHTML = '¡Tiempo!'
+    document.querySelector('.progressbar').classList.add('hidden');
+    clearInterval(intervalID);
+}
+
+function printQuestion(question) {
+    const questionText = document.querySelector('.question')
+    const form = document.querySelector('.main-questions form')
+    const radios = document.querySelectorAll('input[type=radio]')
+    const progress = document.querySelector('progress')
+    const labels = document.querySelectorAll('label')
+    radios.forEach(radio => radio.remove())
+    labels.forEach(label => label.remove())
+    questionText.innerHTML = showQuestion(question)
+    document.querySelector('.progressbar').classList.remove('hidden');
+
+    shuffle(question.answers).forEach(answer => {
+        var input = document.createElement('input'),
+            label = document.createElement('label')
+        label.innerHTML = answer.text
+        input.setAttribute('type','radio')
+        input.setAttribute('name', 'answer')
+        input.id = "answer" + answer.id
+        label.setAttribute('for', "answer" + answer.id)
+        input.value = answer.id
+        form.appendChild(input)
+        form.appendChild(label)
+    })
+    let index = 20;
+    progress.value = index;
+    let intervalID = setInterval(() => {
+        if(index > 0) {
+            index -= 0.01;
+            progress.value = index;
+        }
+        else {
+            document.querySelectorAll('input[type=radio]')
+            .forEach(radio => radio.setAttribute('disabled', true))
+            clearInterval(intervalID);
+        }
+    }, 10);
 }
